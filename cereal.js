0f0ba52f978a825ba1946ba96c74a0ad1e1b80be
Ali Shalabi <ashalabi13@gmail.com>
2020-02-24 12:24:29 -0800

    Have super basic SVF working

--- cereal.js
+++ cereal.js
@@ -6,11 +6,36 @@ d3.csv("cereal.csv")
   .then(function(data) {
     console.log(data)
 
+    const byBrand = {}
+    data.forEach((cereal) => {
+      if (byBrand[cereal.mfr] === undefined) {
+        byBrand[cereal.mfr] = { name: cereal.mfr, children: [] }
+      }
+      byBrand[cereal.mfr].children.push(cereal)
+    })
+
+    console.log(byBrand)
+
     // Instantiating all properties we will use
     const names = data.map(a => a.name)
+    const manufacturer = data.map(a => a.mfg)
+    const type = data.map(a => a.type)
+    const calories = data.map(a => a.calories)
+    const proteins = data.map(a => a.protein)
+    const fats = data.map(a => a.fat)
+    const sodiums = data.map(a => a.sodium)
+    const fibers = data.map(a => a.fiber)
+    const carbos = data.map(a => a.carbo)
     const sugars = data.map(a => a.sugars)
+    const postasses = data.map(a => a.potass)
+    const vitamins = data.map(a => a.vitamins)
+    const shelf = data.map(a => a.shelf)
+    const weights = data.map(a => a.weight)
+    const cups = data.map(a => a.cups)
+    const ratings = data.map(a => a.rating)
     // ...
 
+    // Index All Cereal Names
     const allCereals = document.getElementById("all-cereals")
     names.forEach(name => {
       const el = document.createElement("p")
@@ -18,6 +43,22 @@ d3.csv("cereal.csv")
       el.innerHTML = name + " - "
     })
 
+
+    d3.select('#svg-circles')
+      .style('border', '1px solid #f0f')
+      .selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('r', (d, i) => {
+        return d.protein * 5
+      })
+      .attr('cx', (d, i) => {
+        return i * 400/data.length
+      })
+      .attr('cy', (d, i) => {
+        return i * 400/data.length
+      })
 
 
     // d3.select('#svg-names')
